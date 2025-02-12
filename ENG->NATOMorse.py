745d9d97acbc61768182929b071c750e5ce1b1db
Squiddy <49382935+ASquidboi@users.noreply.github.com>
2025-02-12 09:34:52 -0800

    Create ENG->NATOMorse.py

---
+++ ENG->NATOMorse.py
@@ -0,0 +1,13 @@
+#Basic ENG to NATO Morse Code
+standard = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"]
+output = []
+while True:
+  str = input("Input, no ASCII")
+  res = [char for char in str]
+  output = []
+  for char in res:
+    index = standard.index(char)
+    x = morse[index]
+    output.append(x)
+  print(' '.join(output))
