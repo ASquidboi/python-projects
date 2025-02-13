346e8f549c55d1e9218ee03851f4e8a028504a2f
Squiddy <49382935+ASquidboi@users.noreply.github.com>
2025-02-13 09:20:46 -0800

    Update Encrypter.py

--- Encrypter.py
+++ Encrypter.py
@@ -2,8 +2,10 @@
 standard = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 val = 0
 keyval = 0
+
 while True:
   choice = input("ENCRYPT or DECRYPT")
+  choice = choice.upper()
   if choice == "ENCRYPT":
     msg = input("What is your message?") #not to be confused with Monosodium glutamate
     
